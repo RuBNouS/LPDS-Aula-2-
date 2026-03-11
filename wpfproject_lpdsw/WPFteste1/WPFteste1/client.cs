014db0b32a59d2255eaaa69f91d769cacd00aad7
Ruben <rubensousa.fantasmax@gmail.com>
2026-03-11 15:16:19 +0000

    Adicionar WPF e client

---
+++ wpfproject_lpdsw/WPFteste1/WPFteste1/client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfProjectLpdsw.Models
+{
+    internal class Client
+    {
+        public int Id { get; set; }
+        public string ClientName { get; set; }
+        public string ClientEmail { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+    }
+}
